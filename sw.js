6e238c5713f3dddf8e0da2aed63c050b3f91de7b
majed9m <majed9m@gmail.com>
2026-03-01 23:34:39 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('install', e => {
+const CACHE = "flashcards-anki-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((resp) => {
+      const copy = resp.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return resp;
+    }).catch(() => caches.match("./")))
+  );
 });
